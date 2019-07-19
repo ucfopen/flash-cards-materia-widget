76befc83dad6bd604f75e35db3f90adc6f5140f7
Weining Li <weining.li@ucf.edu>
2019-07-19 10:43:56 -0400

    updated webpack.config.js and lockfile

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
-const marked = require('meta-marked')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const outputPath = path.join(process.cwd(), 'build')
 
 const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
-const ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
 const copy = widgetWebpack.getDefaultCopyList()
 
 //pass in extra files for webpack to copy
@@ -66,55 +63,6 @@ let options = {
 	copyList: customCopy,
 }
 
-const generateHelperPlugin = name => {
-	const file = fs.readFileSync(path.join(__dirname, 'src', '_guides', name+'.md'), 'utf8')
-	const content = marked(file)
+let buildConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
 
-	return new HtmlWebpackPlugin({
-		template: path.join(__dirname, 'src', '_guides', 'helperTemplate'),
-		filename: path.join(outputPath, 'guides', name+'.html'),
-		title: name.charAt(0).toUpperCase() + name.slice(1),
-		chunks: ['guides'],
-		content: content.html
-	})
-}
-
-let webpackConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
-
-const modernizrConfig = {
-	noChunk: true,
-	filename: 'assets/js/modernizr.js',
-	'options':[
-		'domPrefixes',
-		'prefixes',
-		"testAllProps",
-		"testProp",
-		"testStyles",
-		"html5shiv",
-		"load"
-	],
-	'feature-detects': [
-		"touchevents",
-		"svg/inline",
-		"svg",
-		"svg/clippaths",
-		'css/animations',
-		'css/transforms',
-		'css/transforms3d',
-		'css/transitions',
-		'css/generatedcontent',
-		'css/opacity',
-		'css/rgba'
-	],
-	minify: {
-		output: {
-			comments: true,
-			beautify: false
-		}
-	}
-}
-webpackConfig.plugins.unshift(generateHelperPlugin('creator'))
-webpackConfig.plugins.unshift(generateHelperPlugin('player'))
-webpackConfig.plugins.push(new ModernizrWebpackPlugin(modernizrConfig))
-
-module.exports = webpackConfig
+module.exports = buildConfig
